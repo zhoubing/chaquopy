b82e08de2ac2d4579647554c4a5803ac9cbbda46
zhoubing <zhoubing19820720@hotmail.com>
2023-01-14 16:31:49 +0800

    Create MainActivity.java

--- app/src/main/java/com/hifly/chaquopy/MainActivity.java
+++ app/src/main/java/com/hifly/chaquopy/MainActivity.java
@@ -1,13 +1,40 @@
 package com.hifly.chaquopy;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 
+import java.lang.annotation.Annotation;
 import java.util.function.Function;
+
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+class A implements Function<String, String> {
+
+    @Override
+    public String apply(String s) {
+        System.out.println(s);
+        return "msg from Java!!!";
+    }
+}
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+class B implements Function<PyObject, Person> {
+
+    @Override
+    public Person apply(PyObject s) {
+        System.out.println(s.asMap());
+        Person p = new Person();
+        p.setName("Java Namenm");
+        return p;
+    }
+}
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,19 +54,15 @@ public class MainActivity extends AppCompatActivity {
         }.start();
 //        Log.d("chaquopy", pyObject.toString());
         //register python callback
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            pythonModule.put("java_callback", (Function<String, String>) o -> {
-                System.out.println(o);
-                return "msg from Java!!!";
-            });
-        }
-        PyObject pyObject1 = pythonModule.callAttr("hello_world", "King of !!!");
-        System.out.println(pyObject1.toString());
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+//            A a = new A();
+//            pythonModule.put("java_callback", a);
+//        }
+//        PyObject pyObject1 = pythonModule.callAttr("hello_world", "King of !!!");
+//        System.out.println(pyObject1.toString());
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             pythonModule.put("pass_object_callback", (Function<Object[], Person>) o -> {
-//                Person p = o.toJava(Person.class);
-//                System.out.println(p);
                 for (Object o1 : o) {
                     System.out.println(o1);
                 }
@@ -47,10 +70,11 @@ public class MainActivity extends AppCompatActivity {
                 p.setName("Java Name");
                 return p;
             });
+            B b = new B();
+            pythonModule.put("pass_object_callback", b);
         }
         PyObject pyObject2 = pythonModule.callAttr("pass_object");
-//        pyObject2.toJava()
-//        System.out.println(pyObject2.toJava(Person.class));
+        System.out.println(pyObject2.asMap());
     }
 }
 
